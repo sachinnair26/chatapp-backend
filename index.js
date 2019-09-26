46868c0af8b3e1e8676a5b1c7f8a9b8da9a5206d
sachinnair26 <sachin.nair32@gmail.com>
2019-09-26 21:56:22 +0530

    on this point socket io is working

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ var http = require('http');
 var url = require('url');
 var saveuser= require('./saveuserdetail.js');
 var getcontacts = require('./getcontacts');
+var socket = require('socket.io')
 const server = http.createServer((req,res) =>{
     
     const reqUrl = url.parse(req.url,true)
-    console.log(reqUrl);
     
     if(reqUrl.pathname === '/saveuser' && req.method === 'POST'){
         saveuser.saveuserdetail(req,res);
     }else if(reqUrl.pathname === '/home' && req.method === 'GET'){
+        var io = socket.listen(server) 
+        
+        io.on('connect',function(socket) {
+            console.log('thenga');
+            
+        socket.emit('name',{i:'am'})
+   
+        })
+        
         getcontacts.getcontacts(req,res)
     }
-          
-
+   
+    
+    
 });
-server.listen(8080,'127.0.0.2',()=>{
+
+server.listen(3001,'127.0.0.2',()=>{
     console.log('====================================');
     console.log("server started ");
     console.log('====================================');
