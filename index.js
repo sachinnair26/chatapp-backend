fffb9e5d330e100b256f4774147f3606ef4803fb
sachinnair26 <sachin.nair32@gmail.com>
2019-09-28 22:32:40 +0530

    searchdone

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var saveuser= require('./saveuserdetail.js');
 var getcontacts = require('./getcontacts');
 var socket = require('socket.io');
 var saveMessage = require('./saveMessage');
+var seacrhContact = require('./searchContact');
 const server = http.createServer((req,res) =>{
     
     const reqUrl = url.parse(req.url,true)
@@ -20,6 +21,17 @@ const server = http.createServer((req,res) =>{
             socket.on('message',function(meg){
            saveMessage.saveMessage(meg)
                     
+            })
+            socket.on('contact-search',function(value){
+                var search_res = []
+                seacrhContact(value).then(point =>{
+                   point.forEach(wick =>{
+                       search_res.push(wick.user_name)
+                   }).then(tail=>{
+                       socket.emit('search-result',search_res)
+                   })
+                    
+                })
             })
    
         })
---
+++ searchContact.js
@@ -0,0 +1,10 @@
+var mongoconnect = require('./constants').mongoconnect
+
+module.exports = function(search_query){
+    return mongoconnect.then(db =>{
+        return db.db('test').
+        collection('User').
+        find({ user_name: { '$regex': '^'+search_query,'$options':'i' } })
+       
+    })
+}
